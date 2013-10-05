dccef571c2c73e9f560433746cf7e929161147c9
Yuriy <ruzik1014@mail.ru>
2013-10-05 02:12:56 -0700

    Create Duskwood.sql

---
+++ Quests/Eastern-Kingdoms/Duskwood.sql
@@ -0,0 +1,5 @@
+-- фхтагн воргену ауры невидимости
+
+UPDATE creature_template_addon SET auras=NULL WHERE entry=43730;
+UPDATE creature_template_addon SET auras=NULL WHERE entry=288;
+UPDATE creature_template_addon SET auras=NULL WHERE entry=43861;
