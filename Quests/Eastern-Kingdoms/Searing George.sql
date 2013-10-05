089d8fc0eda07093332c9c1790b47e201c144bd4
Yuriy <ruzik1014@mail.ru>
2013-10-05 03:49:53 -0700

    Create Searing George.sql

---
+++ Quests/Eastern-Kingdoms/Searing George.sql
@@ -0,0 +1,4 @@
+--Rise, Obsidion
+
+UPDATE `gossip_menu_option` SET `cond_1_val_1` = '28056' WHERE `menu_id` = '1282' AND `id` = '0'; 
+UPDATE `conditions` SET `value1` = '28056' WHERE `condition_entry` = '896'; 
